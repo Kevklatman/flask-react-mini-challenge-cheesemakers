3a526ce6f1a627d2d552e7797f11424038382eb7
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-22 14:59:37 -0600

    created models/ back populations

--- server/app.py
+++ server/app.py
@@ -6,6 +6,8 @@ from models import Cheese, Producer, db
 # from flask_restful import Api, Resource
 
 
+
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -17,6 +19,7 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 # api = Api(app)
+
 
 
 @app.route("/")
--- server/models.py
+++ server/models.py
@@ -5,11 +5,35 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+from sqlalchemy import MetaData
+...
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata, engine_options={"echo": True})
+
+
 
 class Producer(db.Model, SerializerMixin):
     __tablename__ = "producers"
 
     id = db.Column(db.Integer, primary_key=True)
+    #adding columns
+    founding_year = (db.Integer)
+    name = (db.String)
+    region = (db.String)
+    operation_size = (db.String)
+    image = (db.String)
+
+    # Add the relationship
+    cheeses = db.relationship('Cheese', back_populates='producer')
 
     def __repr__(self):
         return f"<Producer {self.id}>"
@@ -19,6 +43,17 @@ class Cheese(db.Model, SerializerMixin):
     __tablename__ = "cheeses"
 
     id = db.Column(db.Integer, primary_key=True)
+    #adding columns
+    producer_id = (db.Integer, db.ForeignKey('producers.id'))
+    kind = (db.String)
+    is_raw_milk = (db.Boolean)
+    production_date = (db.DateTime)
+    image = (db.String)
+    price = (db.Float)
+
+    # Add the relationship
+    producer = db.relationship('Producer', back_populates='cheeses')
+
 
     def __repr__(self):
         return f"<Cheese {self.id}>"
